fa6fa8f4faab16c31791793badcaeb04f97b7e88
mako(Java2022.11) <118739580+mako719@users.noreply.github.com>
2024-04-26 20:09:53 +0900

    HEBS-17 configヘルパに変更

--- self-management-api/app/Http/Middleware/AuthenticateAPIRequest.php
+++ self-management-api/app/Http/Middleware/AuthenticateAPIRequest.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Config;
 
 class AuthenticateAPIRequest
 {
@@ -20,7 +19,7 @@ class AuthenticateAPIRequest
         // リクエストからsecret keyを取得
         $secretKey = $request->bearerToken();
         // configから登録したsecret keyを取得
-        $registeredSecretKey = Config::get('app.api_secret_key');
+        $registeredSecretKey = config('app.api_secret_key');
         // secret keyが一致しない場合はエラーレスポンスを返す
         if ($secretKey !== $registeredSecretKey) {
             return response()->json(['error' => 'Invalid credentials'], 401);
